cc473347f8cc17b55eb9291e7fe2b0bff72b5d43
Tharindu-Lakshan-Dev <tharinduln2004@gmail.com>
2025-05-02 20:59:03 +0530

    remove NotFound page

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   const { theme, toggleTheme } = useTheme()
   
   const navItems = [
-    { name: 'Home', path: '/' },
+    { name: 'Home', path: '/*' },
     { name: 'About', path: '/about' },
     { name: 'Services', path: '/services' },
     { name: 'Portfolio', path: '/portfolio' },
