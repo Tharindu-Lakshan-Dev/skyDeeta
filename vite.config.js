0b19aae3e50a01c3fde7fd86be783c767527d72d
Tharindu-Lakshan-Dev <tharinduln2004@gmail.com>
2025-05-02 11:37:14 +0530

    Fixed Error

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base:"/skyDeeta/",
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'dist/index.html',
+          dest: '.', // copy to dist/
+          rename: '404.html',
+        },
+      ],
+      watch: {},
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      input: 'index.html',
+    },
+  },
 })
